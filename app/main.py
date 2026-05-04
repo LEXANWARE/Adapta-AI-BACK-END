f5859f29bac23922af56427ddccab8d580cb8a0a
Alex <alexandrepinto586@gmail.com>
2026-05-03 22:46:44 -0300

    feat: mais correções no cors

--- app/main.py
+++ app/main.py
@@ -23,11 +23,12 @@ app = FastAPI(
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://adapta-ai-curriculo.lovable.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://adapta-ai-curriculo.lovable.app"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
